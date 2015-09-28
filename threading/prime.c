26c28494b2b321edae74084610fa91f5ee19c8ec
Mark Hendriks <mark.hendriks@castawaylabs.com>
2015-09-28 12:34:10 +0200

    Add threading template

---
+++ threading/prime.c
@@ -0,0 +1,52 @@
+/* 
+ * Operating Systems  (2INC0)   Practical Assignment
+ * Threaded Application
+ *
+ * STUDENT_NAME_1 (STUDENT_NR_1)
+ * STUDENT_NAME_2 (STUDENT_NR_2)
+ *
+ * Grading:
+ * Students who hand in clean code that fully satisfies the minimum requirements will get an 8. 
+ * ”Extra” steps can lead to higher marks because we want students to take the initiative. 
+ * Extra steps can be, for example, in the form of measurements added to your code, a formal 
+ * analysis of deadlock freeness etc.
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>     // for usleep()
+#include <time.h>       // for time()
+
+#include "prime.h"
+
+static void rsleep (int t);
+
+int main (void)
+{
+    // TODO: start threads generate all primes between 2 and NROF_SIEVE and output the results
+    // (see thread_malloc_free_test() and thread_mutex_test() how to use threads and mutexes,
+    //  see bit_test() how to manipulate bits in a large integer)
+
+
+    return (0);
+}
+
+/*
+ * rsleep(int t)
+ *
+ * The calling thread will be suspended for a random amount of time between 0 and t microseconds
+ * At the first call, the random generator is seeded with the current time
+ */
+static void rsleep (int t)
+{
+    static bool first_call = true;
+    
+    if (first_call == true)
+    {
+        srandom (time (NULL) % getpid());
+        first_call = false;
+    }
+    usleep (random () % t);
+}
+
